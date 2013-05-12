8286e2948dc102c2fb816e6cbc98d357b47dac9c
Irvin Ocanto <iocanto@IdeaPad-z570.(none)>
2013-05-12 12:39:18 -0400

    Migrate to Express

--- web.js
+++ web.js
@@ -1,8 +1,25 @@
-var connect = require('connect'),
-    port    = process.env.PORT || 8001;
+var port  = process.env.PORT || 8001;
 
-connect.createServer(
-    connect.static(__dirname + '/public')
-).listen( port );
+
+var express = require('express')	
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(port);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 
 console.log("Listening on " + port);
